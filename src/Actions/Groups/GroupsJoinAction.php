98e35d116cc5aff10fa5cbb97e8c5d82507eb786
ElectroluxV2 <electroluxv2@gmail.com>
2021-06-12 16:33:56 +0200

    Inform owner about join

--- src/Actions/Groups/GroupsJoinAction.php
+++ src/Actions/Groups/GroupsJoinAction.php
@@ -18,8 +18,9 @@ class GroupsJoinAction extends Action {
         $hash = $this->args["hash"];
 
         $group = $this->medoo->get("groups", [
-           "group_id",
-           "name"
+            "owner_id",
+            "group_id",
+            "name"
         ], [
             "group_id" => $group_id
         ]);
@@ -38,11 +39,27 @@ class GroupsJoinAction extends Action {
             "hash" => $hash
         ]);
 
+        // Check if user is already member of this group
+        if ($this->medoo->has("members", [
+            "user_id" => $_SESSION["user"]["user_id"],
+            "group_id" => $group["group_id"]
+        ])) {
+            throw new Exception("You are already a member of this group!");
+        }
+
         // Add to members
         $this->medoo->insert("members", [
            "user_id" => $_SESSION["user"]["user_id"],
            "group_id" => $group["group_id"]
         ]);
+
+        // Inform owner
+        $groupSettingsUrl = RouteContext::fromRequest($this->request)->getRouteParser()->fullUrlFor($this->request->getUri() ,"Groups Settings", [
+            "group_id" => $group["group_id"],
+            "group_name" => $this->slugs($group["name"])
+        ]);
+
+        $this->push($group["owner_id"], $_SESSION["user"]["given_name"]." ".$_SESSION["user"]["family_name"]." joined your group!", $groupSettingsUrl);
 
         // Forward to Group View
         return $this->response->withHeader("Location", RouteContext::fromRequest($this->request)->getRouteParser()->fullUrlFor($this->request->getUri(),"Groups View", [
